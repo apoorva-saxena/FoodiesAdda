487a98a5cdf66bc15c449086b3d6b7f8443af6eb
apsaxena24 <apsaxena24@gmail.com>
2016-08-20 21:48:29 +0100

    third test passes :zap:

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,4 +2,17 @@ class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
   end
+  def new
+  end
+
+  def create
+    Restaurant.create(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name)
+  end
 end
